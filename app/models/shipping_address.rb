ab8ac9a1ced788a248e82ca9c06c9e47a8010ae0
SelinaChen07 <yingnachen@gmail.com>
2017-10-16 16:18:12 +1100

    Revise shipping address model validation message, revise shipping address new/edit form to show error message individually

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,18 +1,17 @@
 class ShippingAddress < ApplicationRecord
 	has_many :orders, inverse_of: :shipping_address
 
-	validates :firstname, :lastname, :street_address, :suburb_or_city, :state, :country, :postcode,  presence: true, length: {maximum: 255}
+	validates :firstname, :lastname, :street_address, :suburb_or_city, :state, :country, :postcode,  presence:{:message => "Please complete this field to continue."}
+	validates :firstname, :lastname, :email, :level_or_suite, :street_address, :suburb_or_city, :state, :country, :postcode,  length: {maximum: 255, :message => "Maximum 255 charaters allowed."}
 	
 	# Validate australian phone number. Supported formats: 0411 234 567, +61411 234 567, (02) 3892 1111, 38921111, 3892 111, 02 3892 1111.
 	VALID_PHONE_REGEX = /\A\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}\z/;
-	validates :phone, presence: true, length:{maximum: 30}
-	validates :phone, format:{with: VALID_PHONE_REGEX}, :allow_blank => true
+	validates :phone, presence:{:message => "Please complete this field to continue."}
+	validates :phone, format:{with: VALID_PHONE_REGEX, message:"Please enter a valid phone number."}, :allow_blank => true
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates(:email, presence: true, length: {maximum: 255}, 
-	validates :email, format: {with: VALID_EMAIL_REGEX}), :allow_blank=> true
-
-	validates :level_or_suite, length: {maximum: 255}
+	validates :email, presence:{:message => "Please complete this field to continue."}
+	validates :email, format:{with: VALID_EMAIL_REGEX, message:"Please enter a valid email address."}, :allow_blank=> true
 
 	before_save{
 		self.firstname.capitalize!
